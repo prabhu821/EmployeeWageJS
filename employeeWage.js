da7106b206ad6cf9255009a86fe2c5f65462e3f8
rishiraj007 <rishiraj@mounttalent.com>
2023-01-22 21:08:55 +0530

    [Prabhu] Add: Monthly Wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,7 +1,13 @@
-const IS_PART_TIME = 1,IS_FULL_TIME = 2,WAGE_PER_HR = 20,PART_TIME_HOURS = 4,FULL_TIME_HOURS = 8;
-let empCheck = Math.floor(Math.random() * 3 );  
-let empWage = WAGE_PER_HR * GetWorkingHrs(empCheck);
-console.log("Employee wage for this day is "+ empWage);
+const IS_PART_TIME = 1,IS_FULL_TIME = 2,WAGE_PER_HR = 20,PART_TIME_HOURS = 4,FULL_TIME_HOURS = 8,TOTAL_WORKING_DAYS=20;;
+let empWage,totalWorkingHours=0;
+for(let day=1; day<= TOTAL_WORKING_DAYS; day++)
+{
+    let empCheck = Math.floor(Math.random() * 3 ); 
+    totalWorkingHours +=GetWorkingHrs(empCheck);
+    
+}
+empWage = WAGE_PER_HR * totalWorkingHours;
+console.log("Employee Monthly wage is "+ empWage+"\n"+"Total Hours Worked:"+totalWorkingHours);
 
 //Method to get Work Hours
 function GetWorkingHrs(empCheck){
