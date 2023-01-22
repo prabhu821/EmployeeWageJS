6a5eb7137feadfd36169f58329f9800fba3f8b31
rishiraj007 <rishiraj@mounttalent.com>
2023-01-22 21:12:35 +0530

    [Prabhu] Add: Wage till Total Working Hours

--- employeeWage.js
+++ employeeWage.js
@@ -1,13 +1,13 @@
-const IS_PART_TIME = 1,IS_FULL_TIME = 2,WAGE_PER_HR = 20,PART_TIME_HOURS = 4,FULL_TIME_HOURS = 8,TOTAL_WORKING_DAYS=20;;
-let empWage,totalWorkingHours=0;
-for(let day=1; day<= TOTAL_WORKING_DAYS; day++)
+const IS_PART_TIME = 1,IS_FULL_TIME = 2,WAGE_PER_HR = 20,PART_TIME_HOURS = 4,FULL_TIME_HOURS = 8,TOTAL_WORKING_DAYS=20,TOTAL_WORKING_HOURS=100;
+let empWage,totalWorkingDays=0,totalWorkingHours=0;
+while(totalWorkingHours<=TOTAL_WORKING_HOURS && totalWorkingDays<TOTAL_WORKING_DAYS)
 {
     let empCheck = Math.floor(Math.random() * 3 ); 
     totalWorkingHours +=GetWorkingHrs(empCheck);
-    
+    totalWorkingDays++;
 }
 empWage = WAGE_PER_HR * totalWorkingHours;
-console.log("Employee Monthly wage is "+ empWage+"\n"+"Total Hours Worked:"+totalWorkingHours);
+console.log("Employee Monthly wage is "+ empWage+"\n"+"Total Working Hours:"+totalWorkingHours+"\n"+"Total Working Days:"+totalWorkingDays);
 
 //Method to get Work Hours
 function GetWorkingHrs(empCheck){
