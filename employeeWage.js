f1df59eaf0aa9c403d95690944385c155d815047
rishiraj007 <rishiraj@mounttalent.com>
2023-01-22 21:05:14 +0530

    [Prabhu] Add: Daily Wage using Function

--- employeeWage.js
+++ employeeWage.js
@@ -1,19 +1,19 @@
 const IS_PART_TIME = 1,IS_FULL_TIME = 2,WAGE_PER_HR = 20,PART_TIME_HOURS = 4,FULL_TIME_HOURS = 8;
-let empCheck = Math.floor(Math.random() * 3 ); 
-let empWage, workingHours;          
-switch(empCheck){
-    case IS_FULL_TIME:
-        workingHours = FULL_TIME_HOURS;
-        console.log("Employee is present for full-time");
-        break;
-    case IS_PART_TIME:
-        workingHours = PART_TIME_HOURS;
-        console.log("Employee is Present for part-time");
-        break;
-    default:
-        console.log("Employee is Absent");
-        workingHours = 0;
-        break;
+let empCheck = Math.floor(Math.random() * 3 );  
+let empWage = WAGE_PER_HR * GetWorkingHrs(empCheck);
+console.log("Employee wage for this day is "+ empWage);
+
+//Method to get Work Hours
+function GetWorkingHrs(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+            break;
+    }
 }
-empWage = workingHours * WAGE_PER_HR;
-console.log("Employee Wage: " + empWage);
